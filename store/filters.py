0376fe07ced43a672286854fb3c40c1e4f261b0d
Stanislav Dzugov <dzougov91@gmail.com>
2021-01-23 02:20:04 +0300

    restructure filter

--- store/filters.py
+++ store/filters.py
@@ -1,11 +1,20 @@
 import django_filters
 from django_filters import NumberFilter, CharFilter
-from .models import Product
+from .models import Product, Category
+from django import forms
 
 
 class ProductFilter(django_filters.FilterSet):
-    name = CharFilter(field_name='name', lookup_expr='icontains')
-    price = NumberFilter(field_name='price', lookup_expr='lte')
+    name = CharFilter(field_name='name', lookup_expr='icontains', label=False,
+                      widget=forms.TextInput(attrs={
+                          'placeholder': 'Product name',
+                          'class': 'nameField',
+                      }))
+    price = NumberFilter(field_name='price', lookup_expr='lte', label=False,
+                         widget=forms.NumberInput(attrs={
+                          'placeholder': 'Price',
+                          'class': 'priceField',
+                      }))
 
     class Meta:
         model = Product
